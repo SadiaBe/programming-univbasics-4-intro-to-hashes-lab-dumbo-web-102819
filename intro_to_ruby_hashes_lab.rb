17374232b568145eb7d84fe376b116f6c5a37ee6
Sadia Badhon <sadia.islam.badhon@gmail.com>
2019-10-24 21:27:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,5 +10,20 @@ def pioneer
   { name: "Grace Hopper" }
 end 
 
+def id_generator
+  {id: 6}
+end
+
+def my_hash_creator(key, value)
+  {key => value}
+end
+
+def read_from_hash(hash, key)
+  hash[key]
+end
+
+def update_counting_hash(hash, key)
+  hash[key] ? hash[key] += 1 : hash[key] = 1
+  hash
 
 
