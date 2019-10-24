82972588c0b25e024ecf5dc6fbb4e28a59f860fa
Sadia Badhon <sadia.islam.badhon@gmail.com>
2019-10-24 21:19:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,28 +1,3 @@
- def new_hash 
-   {}
+ def new_hash
+  {}
 end
-
-def my_hash 
-  { "name" => "Sadia", "age" => 23 }
-end
-
-
-def pioneer 
-  {name: "Grace Hopper" }
-end
-
-
-def id_generator 
-  {id: 6}
-end
-
-
-def my_hash_creator(key, value)
-  {key => value}
-end
-
-
-def read_from_hash(hash, key)
-  hash[key]
-end
-
